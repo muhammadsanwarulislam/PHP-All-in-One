086ce6338c66fc6a2cc28e2e37434feeaf2f8063
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-03-04 13:23:50 +0600

    String operations

--- Basic/String Operations/1.php
+++ Basic/String Operations/1.php
@@ -1,28 +1,30 @@
 <?php
 
 //strlen()
-// $message = 'Hello';
-// $length = strlen($message);
-// echo $length;
+$message = 'Hello';
+$length = strlen($message);
+echo "Length of the $message".' is :'.$length.PHP_EOL;
 
 //str_split()
-// $string = "Hello World!";
-// $chars = str_split($string);
-// print_r($chars);
+$string = "Hello World!";
+$chars = str_split($string);
+echo "Spilt of the $string".PHP_EOL;
+print_r($chars);
 
 //implode()
-// $fruits = array("Apple", "Banana", "Orange", "Mango");
-// $fruits_string = implode(", ", $fruits);
-// echo $fruits_string;
+$fruits = array("Apple", "Banana", "Orange", "Mango");
+$fruits_string = implode(", ", $fruits);
+echo "Implode of the array: ". $fruits_string.PHP_EOL;
 
 //strpos()
-// $string = "Hello World!";
-// $pos = strpos($string, "World");
-// echo $pos;
+$string = "Hello World!";
+$pos = strpos($string, "World");
+echo "String position of the $string is: ". $pos.PHP_EOL;
 
 //str_replace()
-// $message = 'The quick brown fox jumps over the lazy dog';
-// $animals = ['fox', 'dog'];
-// $newAnimals = ['cat','cow'];
-// $strReplace = str_replace($animals, $newAnimals, $message);
-// print_r($strReplace);
+$message = 'The quick brown fox jumps over the lazy dog';
+$animals = ['fox', 'dog'];
+$newAnimals = ['cat','cow'];
+$strReplace = str_replace($animals, $newAnimals, $message);
+echo "The main message is: $message".PHP_EOL;
+print_r($strReplace).PHP_EOL;
