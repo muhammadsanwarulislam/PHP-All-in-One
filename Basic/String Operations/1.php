5d16cc38ae30b013ef367fac4d8cd4c64d6e26da
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-03-04 15:23:23 +0600

    Working on string operation

--- Basic/String Operations/1.php
+++ Basic/String Operations/1.php
@@ -24,10 +24,10 @@ echo "String position of the $string is: ". $pos.PHP_EOL;
 //str_replace()
 $message = 'The quick brown fox jumps over the lazy dog';
 $animals = ['fox', 'dog'];
-$newAnimals = ['cat','cow'];
-$strReplace = str_replace($animals, $newAnimals, $message);
+$new_animals = ['cat','cow'];
+$str_replace = str_replace($animals, $new_animals, $message);
 echo "The main message is: $message".PHP_EOL;
-print_r($strReplace).PHP_EOL;
+print_r($str_replace).PHP_EOL;
 
 //trim()
 $str = "   Hello World!   ";
@@ -38,3 +38,22 @@ echo $trimmed.PHP_EOL;
 $str = "apple,banana,orange";
 $fruits = explode(",", $str);
 print_r($fruits).PHP_EOL; 
+
+//Leetcode Problem
+function groupAnagrams($strs) {  
+    $res=[];
+    foreach($strs as $str){
+        $strArray=str_split($str);
+        sort($strArray);
+        $temp=implode(' ',$strArray).'';
+        /*
+        The $res array is an associative array where the keys are the sorted strings 
+        of characters and the values are arrays of strings that are anagrams of each 
+        other. The [] notation after $res[$temp] means that a new element is appended 
+        to the end of the existing array for the corresponding key.
+        */
+        $res[$temp][]=$str;
+    }
+    return $res;
+}
+print_r(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));//[["eat","tea","ate"],["tan","nat"],["bat"]]
