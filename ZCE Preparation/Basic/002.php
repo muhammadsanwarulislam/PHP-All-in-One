b4af62f6fbcbbb6b46d7ec1d7ab907d5ffd16ba6
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-04-25 21:56:09 +0600

    Zend preparation

--- ZCE Preparation/Basic/002.php
+++ ZCE Preparation/Basic/002.php
@@ -3,11 +3,5 @@
 function magic($p, $q) {
     return ($q == 0) ? $p : magic($q, $p % $q);
 }
-$array = array(
-    1    => "a",
-    "1"  => "b",
-    1.5  => "c",
-    true => "d",
-);
-var_dump($array);
+
 print_r(magic(100,20));
---
+++ ZCE Preparation/Basic/006.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($array);
