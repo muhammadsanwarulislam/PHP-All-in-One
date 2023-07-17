916f54175390d92efe36035ce9e937d5f1a0a4bc
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2023-07-17 21:57:36 +0600

    readonly class

---
+++ Basic/008-OPP/001-Basic/2.php
@@ -0,0 +1,15 @@
+<?php
+class ExampleOfReadOnly {
+  public readonly string $name;
+
+  public function __construct(string $name) {
+    $this->name = $name;
+  }
+}
+
+$object = new ExampleOfReadOnly('muhammad');
+// Legal read.
+echo $object->name;
+
+// Illegal reassignment. It does not matter that the assigned value is the same.
+$object->name = 'sanwarul';
