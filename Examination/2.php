43f0574c7f45df7364b754dc1d2e48c8e6f4f01a
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2024-04-28 10:45:40 +0600

    remove all occurrences

--- Examination/2.php
+++ Examination/2.php
@@ -1,16 +1,20 @@
-<?php
-function isPalindrome($str) {
-    $cleanedStr = strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $str));
+<?php 
+//  Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val. 
+ 
 
-    $reversedStr = strrev($cleanedStr);
-    
-    return $cleanedStr === $reversedStr;
-}
+ function removeElement($nums, $val){
+    $result = [];
+    foreach($nums as $num){
+        if($num != $val){
+            // 1st way
+            $result[] = $num;
+            //second way
+            // array_push($result,$num);
+        }
+    }
+    return $result;
+ }
 
-
-$input1 = "carrace";
-$input2 = "abcd";
-
-echo "$input1 is " . (isPalindrome($input1) ? "" : "not ") . "a palindrome.\n";
-echo "$input2 is " . (isPalindrome($input2) ? "" : "not ") . "a palindrome.\n";
-
+ $nums = [3,2,2,3,2];
+ $val  = 3;
+ print_r(removeElement($nums,$val));
