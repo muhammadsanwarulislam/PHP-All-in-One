ad71954e05f717926e29768f4ee3ea459a59cb82
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2024-05-02 11:46:17 +0600

    in_array

---
+++ Basic/010-Array/(3)in_array.php
@@ -0,0 +1,10 @@
+<?php 
+
+$nums = [1,3,4,6];
+$checkItem = 0;
+
+if(in_array($checkItem,$nums)){
+    echo $checkItem." is exist the array".PHP_EOL;
+}else{
+    echo $checkItem." is not exist in the array".PHP_EOL;
+}
